af28663ec12d91cc69815052baa39fd800904319
ductrung794 <trung070904@gmail.com>
2025-10-02 14:22:21 +0700

    plus

---
+++ Caculate.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exe;
+
+public class Caculate {
+    private Double caculate(SIGN sign, Double arg1, Double arg2){
+        switch (sign) {  
+            case Plus: 
+                return arg1 + arg2;      
+            default:
+                break;
+        }
+        return 0d;
+    }
+}
